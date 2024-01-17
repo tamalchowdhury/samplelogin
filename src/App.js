6d0ae46b8817b482bfbeeb83ec39d53eee2e7b99
Tamal Chowdhury <tamalanwaronline@gmail.com>
2024-01-17 22:44:32 +0600

    sample login and registration form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
 
-function App() {
+function Form({ handleForm, submitText }) {
+  function handleFormSubmit(event) {
+    event.preventDefault()
+    const { username, password } = event.target.elements
+
+    handleForm({
+      username: username.value,
+      password: password.value,
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          <input type="text" id="username" placeholder="username" />
+        </div>
+        <div>
+          <input type="password" name="password" id="password" />
+        </div>
+        <button type="submit">{submitText}</button>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+function App() {
+  function login(formData) {
+    console.log("login", formData)
+  }
+
+  function register(formData) {
+    console.log("register", formData)
+  }
+
+  return (
+    <div className="App">
+      <h1>Sample Application</h1>
+      <p>Hello this is my website, how are you doing?</p>
+      <h3>Login</h3>
+      <Form handleForm={login} submitText="Login" />
+
+      <h3>Register</h3>
+      <Form handleForm={register} submitText="Register" />
+    </div>
+  )
+}
+
+export default App
