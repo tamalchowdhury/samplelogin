0fb27c456270735395149c726d1ba33a6015b56e
Tamal Anwar Chowdhury <tamalanwaronline@gmail.com>
2024-01-19 18:25:17 +0600

    removes auto0

--- src/index.js
+++ src/index.js
@@ -3,20 +3,11 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { Auth0Provider } from '@auth0/auth0-react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Auth0Provider
-      domain="dev-mgd9m-c1.us.auth0.com"
-      clientId="lB64fgFB9XkcuO5oUQvjQaEav1I5xSdk"
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-      }}
-    >
-      <App />
-    </Auth0Provider>
+    <App />
   </React.StrictMode>
 )
 
