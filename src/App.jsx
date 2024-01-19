fe26ddf3eea368709fbcdb6d4309b8ac791da2a2
Tamal Anwar Chowdhury <tamalanwaronline@gmail.com>
2024-01-19 21:34:16 +0600

    adds env variables

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,40 @@ import './App.css'
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from 'firebase/auth'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyAe5hM_oNjeizF2ESBGTtl8bWtzUvCCatE',
-  authDomain: 'simple-login-4ebae.firebaseapp.com',
-  projectId: 'simple-login-4ebae',
-  storageBucket: 'simple-login-4ebae.appspot.com',
-  messagingSenderId: '746918235872',
-  appId: '1:746918235872:web:24410233850db1aa164a85',
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGE_SENDER_ID,
+  appId: process.env.APP_ID,
 }
 
-function Form({ handleForm, submitText }) {
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+
+function Form({ formFn, formText }) {
   function handleFormSubmit(event) {
     event.preventDefault()
     const { email, password } = event.target.elements
 
-    handleForm({
+    const formData = {
       email: email.value,
       password: password.value,
-    })
+    }
+
+    formFn(formData)
   }
 
   return (
@@ -37,39 +48,86 @@ function Form({ handleForm, submitText }) {
         <div>
           <input type="password" name="password" id="password" />
         </div>
-        <button type="submit">{submitText}</button>
+        <button type="submit">{formText}</button>
       </form>
     </div>
   )
 }
 
 function App() {
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig)
-  const auth = getAuth(app)
+  const [user, setUser] = React.useState(null)
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false)
+  const [isLoading, setIsLoading] = React.useState(false)
+  const [error, setError] = React.useState(null)
 
-  function login(formData) {
-    console.log('login', formData)
-  }
+  React.useEffect(() => {
+    const user = auth.currentUser
+
+    if (!user) return
+
+    setUser(user)
+    setIsLoggedIn(true)
+  }, [user])
 
   function register(formData) {
-    createUserWithEmailAndPassword(auth, formData.email, formData.password)
-      .then((userCredential) => {
-        const { user } = userCredential
-        console.log(user)
+    const { email, password } = formData
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userData) => {
+        const { user } = userData
+        setUser(user)
       })
-      .catch((err) => err.message)
+      .catch((err) => {
+        setError(err)
+        console.log(err.message)
+      })
+  }
+
+  function login(formData) {
+    const { email, password } = formData
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userData) => {
+        const { user } = userData
+        setUser(user)
+      })
+      .catch((err) => {
+        setError(err)
+        console.log(err.message)
+      })
+  }
+
+  function logout() {
+    signOut(auth).then(() => {
+      setUser(null)
+      setIsLoggedIn(false)
+    })
+  }
+
+  function AuthenticatedApp() {
+    return (
+      <>
+        <h3>Hello {user.email}</h3> <button onClick={logout}>Logout</button>
+        <p>Hello this is my website, how are you doing?</p>
+      </>
+    )
+  }
+
+  function NotAuthenticatedApp() {
+    return (
+      <>
+        <h3>Login</h3>
+        <Form formFn={login} formText="Log in" />
+        <h3>Register</h3>
+        <Form formFn={register} formText="Sign Up" />
+      </>
+    )
   }
 
   return (
     <div className="App">
       <h1>Sample Application</h1>
-      <p>Hello this is my website, how are you doing?</p>
-      <h3>Login</h3>
-      <Form handleForm={login} submitText="Login" />
-
-      <h3>Register</h3>
-      <Form handleForm={register} submitText="Register" />
+      {isLoggedIn ? <AuthenticatedApp /> : <NotAuthenticatedApp />}
     </div>
   )
 }
